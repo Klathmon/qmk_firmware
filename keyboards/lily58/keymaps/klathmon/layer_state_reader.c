7d08c413b8c9f80aad1e007d40c6bceabe65e12c
Gregory Benner <gregbenner1@gmail.com>
2020-01-03 15:34:40 -0500

    use correct layer numbers on display

--- keyboards/lily58/keymaps/klathmon/layer_state_reader.c
+++ keyboards/lily58/keymaps/klathmon/layer_state_reader.c
@@ -3,25 +3,21 @@
 #include <stdio.h>
 #include "lily58.h"
 
-#define L_BASE 0
-#define L_MOVEMENT 2
-#define L_NUMPAD 4
-
 char layer_state_str[24] = {};
 
 const char *read_layer_state(void) {
-  switch (layer_state) {
-    case L_BASE:
+  switch (biton32(layer_state)) {
+    case 0:
       snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Main");
       break;
-    case L_MOVEMENT:
+    case 1:
       snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Mvmt & F-keys");
       break;
-    case L_NUMPAD:
+    case 2:
       snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Numpad");
       break;
     default:
-      snprintf(layer_state_str, sizeof(layer_state_str), "Layer: %ld", layer_state);
+      snprintf(layer_state_str, sizeof(layer_state_str), "Layer: %d", biton32(layer_state));
   }
 
   return layer_state_str;
