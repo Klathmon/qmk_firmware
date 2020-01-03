711652ffd4c04319d3783e4b806aa6983b37fc09
Gregory Benner <gregbenner1@gmail.com>
2020-01-03 16:05:48 -0500

    fix keymap and add lock button

--- keyboards/lily58/keymaps/klathmon/keymap.c
+++ keyboards/lily58/keymaps/klathmon/keymap.c
@@ -49,9 +49,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
   [_NUMPAD_LAYER] = LAYOUT(
       KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,                      KC_PAST,  KC_P7,  KC_P8,  KC_P9,  KC_PMNS,  KC_TRNS,
-      KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,                      KC_PSLS,  KC_P4,    KC_P5,  KC_P6,  KC_PPLS,  KC_NO,
-      KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,                      KC_NO,  KC_P1,  KC_P2,  KC_P3,  KC_PENT,  KC_NO,
-      KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_NO,  KC_P0,  KC_P0,  KC_PDOT,  KC_PENT,  KC_TRNS,
+      KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,                      KC_PSLS,  KC_P4,  KC_P5,  KC_P6,  KC_PPLS,  KC_NO,
+      KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,                      KC_NO,    KC_P1,  KC_P2,  KC_P3,  KC_PENT,  KC_NO,
+      KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_NO,    KC_P0,  KC_P0,KC_PDOT,  KC_PENT,  KC_TRNS,
                                     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS
   ),
 };
@@ -178,6 +178,14 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       return false;
       break;
+    case LCK_SCRN:
+      if (record->event.pressed) {
+        if (get_mac_mode()) {
+          tap_code16(LGUI(LCTL(KC_Q)));
+        } else {
+          tap_code16(LGUI(KC_L));
+        }
+      }
   }
   return true;
 }
