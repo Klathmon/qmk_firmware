64638567679b0420e6ec32fd185acdc920ec5ff4
Gregory Benner <greg.benner@winmore.app>
2020-01-10 16:36:47 -0800

    cleanup and key rewriting simplification

--- keyboards/lily58/keymaps/klathmon/keymap.c
+++ keyboards/lily58/keymaps/klathmon/keymap.c
@@ -23,14 +23,8 @@ extern uint8_t is_master;
 #define _HYPER_LAYER 3
 
 enum custom_keycodes {
-  MAIN_L = SAFE_RANGE,
-  MVMT_L,
-  NPD_L,
-  HYPR_L, // special layer for advanced and crazy things
-  MAC_MODE, // toggle between MacOS and Windows compat
+  MAC_MODE = SAFE_RANGE, // toggle between MacOS and Windows compat
   LCK_SCRN, // lock screen
-  L_O_W, // left one word
-  R_O_W, // right one word
   LNCH_APP, // launch the companion app
 };
 
@@ -41,13 +35,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,                         KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSLS,
       KC_LGUI,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,                         KC_H,     KC_J,     KC_K,     KC_L,  KC_SCLN,     KC_QUOT,
       KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,   KC_MUTE,  LCK_SCRN,   KC_N,     KC_M,  KC_COMM,   KC_DOT,  KC_SLSH,      KC_ENT,
-                                    KC_LALT,  KC_ESC,  KC_LCTL,  KC_SPC,   KC_DEL,  MVMT_L,    NPD_L,    HYPR_L
+                                    KC_LALT,  KC_ESC,  KC_LCTL,  KC_SPC,    KC_DEL,  MO(1),    MO(2),    MO(3)
   ),
   [_MOVEMENT_LAYER] = LAYOUT(
-      KC_TRNS,    KC_F1,    KC_F2,    KC_F3,    KC_F4,  KC_TRNS,                      KC_TRNS,  KC_HOME,  KC_TRNS,   KC_END,  KC_MINS,   KC_EQL,
-      KC_TRNS,    KC_F5,    KC_F6,    KC_F7,    KC_F8,  KC_TRNS,                      KC_TRNS,    L_O_W,    KC_UP,    R_O_W,  KC_LBRC,  KC_RBRC,
-      KC_TRNS,    KC_F9,   KC_F10,   KC_F11,   KC_F12,  KC_TRNS,                      KC_PGUP,  KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_LCBR,  KC_RCBR,
-      KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS , KC_PGDN,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_TRNS,  KC_TRNS,
+      KC_TRNS,    KC_F1,    KC_F2,    KC_F3,    KC_F4,  KC_TRNS,                      KC_TRNS,  KC_HOME,  KC_PGUP,   KC_END,  KC_MINS,   KC_EQL,
+      KC_TRNS,    KC_F5,    KC_F6,    KC_F7,    KC_F8,  KC_TRNS,                      KC_TRNS,LCTL(KC_LEFT), KC_UP,LCTL(KC_RIGHT), KC_LBRC, KC_RBRC,
+      KC_TRNS,    KC_F9,   KC_F10,   KC_F11,   KC_F12,  KC_TRNS,                      KC_TRNS,  KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_LCBR,  KC_RCBR,
+      KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS , KC_TRNS,  KC_TRNS,  KC_PGDN,  KC_TRNS,  KC_TRNS,  KC_TRNS,
                                     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS
   ),
   [_NUMPAD_LAYER] = LAYOUT(
@@ -61,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,                      KC_TRNS,  KC_TRNS,  RGB_TOG,  RGB_HUI,  RGB_SAI,  RGB_VAI,
       KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,                      KC_TRNS,  KC_TRNS,  RGB_MOD,  RGB_HUD,  RGB_SAD,  RGB_VAD,
       KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,                      KC_TRNS,  KC_TRNS,  RGB_M_P,  KC_TRNS,  KC_TRNS,  KC_TRNS,
-      KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,    RESET,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  LNCH_APP,  MAC_MODE,
+      KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,    RESET,  KC_TRNS,  KC_MPRV,  KC_MPLY,  KC_MNXT, LNCH_APP,  MAC_MODE,
                                     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS
   ),
 };
@@ -87,8 +81,8 @@ const char *read_layer_state(void);
 void set_wpm(void);
 const char *read_wpm_string(void);
 
-void toggle_mac_mode (void);
-bool get_mac_mode (void);
+void toggle_mac_mode(void);
+bool get_mac_mode(void);
 const char *read_mac_mode_state(void);
 
 bool is_hid_connected(void);
@@ -139,73 +133,10 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   uint8_t mods = get_mods();
 
   switch (keycode) {
-    // layers first
-    case MAIN_L:
-      if (record->event.pressed) {
-        set_single_persistent_default_layer(_MAIN_LAYER);
-      }
-      return false;
-      break;
-    case MVMT_L:
-      if (record->event.pressed) {
-        layer_on(_MOVEMENT_LAYER);
-      } else {
-        layer_off(_MOVEMENT_LAYER);
-      }
-      return false;
-      break;
-    case NPD_L:
-      if (record->event.pressed) {
-        layer_on(_NUMPAD_LAYER);
-      } else {
-        layer_off(_NUMPAD_LAYER);
-      }
-      return false;
-      break;
-    case HYPR_L:
-      if (record->event.pressed) {
-        layer_on(_HYPER_LAYER);
-      } else {
-        layer_off(_HYPER_LAYER);
-      }
-      return false;
-      break;
+    // fancy keycodes!
     case MAC_MODE:
       if (record->event.pressed) {
         toggle_mac_mode();
-      }
-      return false;
-      break;
-    // fancy keycodes!
-    case L_O_W:
-      if (record->event.pressed) {
-        if (get_mac_mode()) {
-          register_code16(LALT(KC_LEFT));
-        } else {
-          register_code16(LCTL(KC_LEFT));
-        }
-      } else {
-        if (get_mac_mode()) {
-          unregister_code16(LALT(KC_LEFT));
-        } else {
-          unregister_code16(LCTL(KC_LEFT));
-        }
-      }
-      return false;
-      break;
-    case R_O_W:
-    if (record->event.pressed) {
-      if (get_mac_mode()) {
-          register_code16(LALT(KC_RIGHT));
-        } else {
-          register_code16(LCTL(KC_RIGHT));
-        }
-      } else {
-        if (get_mac_mode()) {
-          unregister_code16(LALT(KC_RIGHT));
-        } else {
-          unregister_code16(LCTL(KC_RIGHT));
-        }
       }
       return false;
       break;
@@ -235,7 +166,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         } else {
           SEND_STRING(SS_LGUI("r"));
           _delay_ms(400);
-          SEND_STRING("cmd /C npx @klathmon/qmk-hid-display"SS_TAP(X_ENT)SS_TAP(X_ENT));
+          SEND_STRING("cmd /C npx @klathmon/qmk-hid-display"SS_TAP(X_ENT));
+          _delay_ms(100);
+          register_code(KC_LGUI);
+          tap_code(KC_DOWN);
+          unregister_code(KC_GUI);
         }
       }
       return false;
@@ -277,6 +212,28 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
           register_code16(LCTL(LGUI(KC_UP)));
         } else {
           unregister_code16(LCTL(LGUI(KC_UP)));
+        }
+        return false;
+      }
+      break;
+    case KC_LEFT:
+      if (get_mac_mode() && mods & MOD_MASK_GUI) {
+        if (record->event.pressed) {
+            register_code16(LALT(KC_LEFT));
+          } else {
+            unregister_code16(LALT(KC_LEFT));
+          }
+        }
+        return false;
+      }
+      break;
+    case KC_RIGHT:
+      if (get_mac_mode() && mods & MOD_MASK_GUI) {
+        if (record->event.pressed) {
+            register_code16(LALT(KC_RIGHT));
+          } else {
+            unregister_code16(LALT(KC_RIGHT));
+          }
         }
         return false;
       }
