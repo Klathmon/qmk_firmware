31d8736445ba4cff176a20471f56bc94d186b029
Gregory Benner <gregbenner1@gmail.com>
2023-07-29 20:10:15 -0400

    fix manufacturers and update Lily58L LED hyper keys

--- keyboards/lily58/light/config.h
+++ keyboards/lily58/light/config.h
@@ -1,5 +1,8 @@
 /*
 Copyright 2012 Jun Wako <wakojun@gmail.com>
+Copyright 2015 Jack Humbert
+Copyright 2017 F_YUUCHI
+Copyright 2020 Ben Roesner (keycapsss.com)
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -19,10 +22,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* USB Device descriptor parameter */
 #define VENDOR_ID       0x6B6C
-#define PRODUCT_ID      0xEB2D
+#define PRODUCT_ID      0x4C4C // "LL"
 #define DEVICE_VER      0x0102
 #define MANUFACTURER    Klathmon
-#define PRODUCT         Lily58
+#define PRODUCT         Lily58L
 
 /* key matrix size */
 // Rows are doubled-up
@@ -33,32 +36,16 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MATRIX_ROW_PINS { C6, D7, E6, B4, B5 }
 #define MATRIX_COL_PINS { F6, F7, B1, B3, B2, B6 }
 
-#define SOFT_SERIAL_PIN D2
-#define SERIAL_USE_MULTI_TRANSACTION
-
-/* define if matrix has ghost */
-//#define MATRIX_HAS_GHOST
-
 /* Set 0 if debouncing isn't needed */
 #define DEBOUNCE 5
 
-/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
-#define LOCKING_SUPPORT_ENABLE
-/* Locking resynchronize hack */
-#define LOCKING_RESYNC_ENABLE
+#define SOFT_SERIAL_PIN D2
 
-/*
- * Feature disable options
- *  These options are also useful to firmware size reduction.
- */
+#define RGB_DI_PIN D3
+#define RGBLED_SPLIT { 35, 35 }
+#define RGBLED_NUM 70
+#define RGBLIGHT_SPLIT
+#define RGBLIGHT_LIMIT_VAL 120
 
-/* disable debug print */
-//#define NO_DEBUG
-
-/* disable print */
-//#define NO_PRINT
-
-/* disable action features */
-//#define NO_ACTION_LAYER
-//#define NO_ACTION_TAPPING
-//#define NO_ACTION_ONESHOT
+#define ENCODERS_PAD_A { F4 }
+#define ENCODERS_PAD_B { F5 }
